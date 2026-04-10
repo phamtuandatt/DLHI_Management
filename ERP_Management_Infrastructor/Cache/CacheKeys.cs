ffce6d9fc397bd47541f8f67f99f8e3b7fa7dd42
DESKTOP-KD2BPDJ\TUAN DAT <phamtuandat16072001@gmail.com>
2026-04-10 21:40:45 +0700

    Add Infrastructor Libary

---
+++ ERP_Management_Infrastructor/Cache/CacheKeys.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP_Management_Infrastructor.Cache
+{
+    public static class CachKeys
+    {
+        public const string SUPPLIER_LIST = "SUPPLIER_List";
+
+        public const string MPRS_LIST = "MPR_List";
+
+        public const string PO_LIST = "PO_List";
+
+        public const string RIR_LIST = "RIR_List";
+
+        public const string PROJECTS_LIST = "PROJECT_List";
+    }
+}
